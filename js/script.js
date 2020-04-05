cb42b4b42adffeba65a1506065192c4f9fd8b4f4
Henry <henrymoire@gmail.com>
2020-04-05 14:05:29 +0300

    Add an AkanName function

--- js/script.js
+++ js/script.js
@@ -17,8 +17,6 @@ function numberOfDay() //Transform the user's birthday to a int
     month += 10;
     decadeYear -= 1;
   }
-  alert(month);
-  alert(decadeYear);
   //To calculate which day of the week the user was born ***Error given with some dates
   var a = ((13 * month - 1) / 5);
   var aInt = Math.trunc(a);
@@ -27,12 +25,6 @@ function numberOfDay() //Transform the user's birthday to a int
   var c = (century / 4);
   var cInt = Math.trunc(c);
   var dayOfTheWeek = (day + aInt + decadeYear + bInt + cInt - 2 * cInt) % 7;
-  /*alert(day);
-  alert(month);
-  alert(year);
-  alert(century);
-  alert(decadeYear);
-  alert(dayOfTheWeek);*/
   return dayOfTheWeek;
 }
 
@@ -63,6 +55,46 @@ function dayOfBirth(dayNumber) //Transform int from numberOfDay function to the 
     	var day = "Sunday";
     	break;
   }
+  alert(day);
   return day;
 }
-alert(dayOfBirth());
+
+function akanName(day)
+{
+  var gender = prompt("What is your gender?");
+  var akanName = "";
+  var dayOfBirth = day;
+  if(gender == "male")
+  {
+    switch(dayOfBirth)
+    {
+        case Monday:
+          akanName = "Kwadwo";
+          break;
+        case Tuesday:
+          akanName = "Kwabena";
+          break;
+        case Wednesday:
+          akanName = "Kwaku";
+          break;
+        case Thursday:
+          akanName = "Yaw";
+          break;
+        case Friday:
+          akanName = "Kofi";
+          break;
+        case Saturday:
+          akanName = "Kwame";
+          break;
+        case Sunday:
+          akanName = "Kwasi";
+          break;
+    }
+  }
+  alert(akanName);
+  return akanName;
+}
+
+var day = dayOfBirth();
+var name = akanName(day);
+alert(name);
